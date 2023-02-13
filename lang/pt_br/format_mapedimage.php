601d7aa1f996471019efc76039b7af51b19ea5f6
Esdras Caleb <esdrascaleb@gmail.com>
2023-02-13 01:03:55 -0300

    fix warning

--- lang/pt_br/format_mapedimage.php
+++ lang/pt_br/format_mapedimage.php
@@ -1,4 +1,4 @@
 <?php
-$string['pluginname'] = 'Formato Imagem Maepada';
+$string['pluginname'] = 'Formato Trilha com Imagem Maepada';
 $string['image'] = 'Imagem Para Mapear';
 $string['editimage'] = 'Modificar Imagem';
